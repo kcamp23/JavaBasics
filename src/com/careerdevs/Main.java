e4d45df88ad11a818980ef54817aea511aa08a77
kcamp23 <89957266+kcamp23@users.noreply.github.com>
2022-01-04 17:08:11 -0500

    2/3  COmmit

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,27 +1,48 @@
 package com.careerdevs;
 
+//class JavaBasics {
+//    public static void main(String [] args ) {
+//        String FirstName = "Kat";
+//        String LastName = "Camp";
+//        String FullName = FirstName +"," + LastName;
+//
+//
+//
+//        System.out.println(FullName );
+//    }
+//}
+
 class JavaBasics {
-    public static void main(String [] args ) {
-        String FirstName = "Kat";
-        String LastName = "Camp";
-        String FullName = FirstName + LastName;
-
-
-
-        System.out.println(FullName );
-    }
-}
-    public class CreateFullName{
-
-    public String FirstName;
-    public String LastName;
-public CreateFullName (String FirstName, String LastName)
+    public static void  main(String [] args) {
+User user =   new User( );
+user.firstName = "Kat";
+user.lastName = "Camp";
+ System.out.println(user.getFullName());
 
     }
-        public static String concatName(String firstName, String lastName) {
+    public class User {
+        public String firstName;
+        public String lastName;
 
+        public String getFullName(){
+            return firstName + "" + lastName;
         }
-    System.out.println(CreateFullName)
-
-    createFullName( "First", "Last")
-    createFullName( "Gabriel", "Sherman")
+    }
+    }
+//
+//    public String FirstName;
+//    public String LastName;
+//public CreateFullName (String FirstName, String LastName)
+//
+//    }
+//        public static String concatName(String firstName, String lastName) {
+//
+//        }
+//    System.out.println(CreateFullName)
+//
+//    createFullName( "First", "Last")
+//    createFullName( "Gabriel", "Sherman")
+//
+//public static String concatName(String firstName, String lastName) {
+//        return String.format("%s, %s", lastName, firstName);
+//        }

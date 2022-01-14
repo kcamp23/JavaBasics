e762c66ce3dfeedf01a89d967c1a82d2a17e3bad
Kat Camp <kcamp552369@gmail.com>
2022-01-14 13:11:58 -0500

    car factory

---
+++ src/com/careerdevs/carfactory/BuildACar.java
@@ -0,0 +1,19 @@
+package com.careerdevs.carfactory;
+
+public class BuildACar {
+
+    public String owner;
+    public String make;
+    public String model;
+    public int  mileage;
+
+
+    public Car (String owner, String make, String model,  Int mileage){
+
+        this.owner = owner;
+        this.make = make;
+        this.model = model;
+        this.mileage = mileage;
+    }
+
+}
---
+++ src/com/careerdevs/carfactory/MainCarFactory.java
@@ -0,0 +1,37 @@
+package com.careerdevs.carfactory;
+
+import java.util.Scanner;
+
+public class MainCarFactory {
+    public static void main(String[] args) {
+        createACar();
+        createACarObject();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is owners name?");
+        String owner = scanner.next();
+
+        System.out.print("What make would you like?");
+        String make = scanner.next();
+
+        System.out.print("What model would you like?");
+        String model = scanner.next();
+
+        System.out.print("What mileage are you hoping for?");
+        int mileage = scanner.nextInt();
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
